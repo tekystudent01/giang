cd82f46a994c1b7cd8622a83fcb0dee7d72b896b
trungvo347 <strungvo@gmail.com>
2023-12-24 11:59:16 +0700

    giangsinh3

--- code.py
+++
@@ -1,8 +0,0 @@
-import turtle 
-screen = turtle.Screen()
-
-screen.setup(1920,1440)
-screen.bgpic('anh-nen-giang-sinh.gif')
-screen.update()
-
-screen.exitonclick
---
+++ code1.py
@@ -0,0 +1,50 @@
+import random
+import turtle 
+screen = turtle.Screen()
+
+
+screen.bgpic('anh-nen-giang-sinh.gif')
+
+
+
+myPen= turtle.Turtle()
+myPen.speed(4000)
+myPen.pensize(3)
+
+
+def addText(message, color, x, y, size):
+    myPen.penup()
+    myPen.goto(x, y)
+    myPen.pendown()
+    myPen.color(color)
+    style = ('Courier', size , 'italic')
+    myPen.write(message, font=style, align= 'center')
+
+
+addText('Happy merry christmas', '#FF0000',0,0,30)
+addText('With your friends and family','#FFFFFF', 0,-30,15)
+
+def hoatuyet():
+    myPen.penup()
+    myPen.goto(random.randint(100,100),random.randint(100,100))
+    myPen.pendown()
+    for i in range (0,8):
+
+        myPen.forward(50)
+        myPen.penup()
+        myPen.backward(15)
+        myPen.left(45)
+        myPen.pendown()
+        myPen.forward(5)
+        myPen.backward(5)
+        myPen.right(90)
+        myPen.forward(5)
+        myPen.backward(5)
+        myPen.left(45)
+        myPen.backward(35)
+        myPen.left(45)
+
+hoatuyet()
+
+screen.update()
+screen.exitonclick()
